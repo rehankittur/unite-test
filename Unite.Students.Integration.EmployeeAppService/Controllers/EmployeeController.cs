8da6b9f4d9b80730ae1043c67311f394f67f00c0
rehan <mohammed.kittur@nttdata.com>
2020-08-24 15:41:09 +0530

    comit
    commit

---
+++ Unite.Students.Integration.EmployeeAppService/Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Unite.Students.Integration.EmployeeAppService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+
+        private readonly ILogger<EmployeeController> _logger;
+
+        public EmployeeController(ILogger<EmployeeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Api is ready !!";
+        }
+    }
+}
