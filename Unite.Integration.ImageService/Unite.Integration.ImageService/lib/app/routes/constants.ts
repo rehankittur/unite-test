413de7e96ebe6d638c5fc15c5f58e46bbb0294cd
rehan <mohammed.kittur@nttdata.com>
2020-08-28 15:44:16 +0530

    confirm
    con

--- Unite.Integration.ImageService/Unite.Integration.ImageService/lib/app/routes/constants.ts
+++
@@ -1,7 +0,0 @@
-export const IMAGE_PROCESS_ROUTES = {
-    TEST: '/test',
-    HEALTH_CHECK: '/health-check',
-    GENERATE_SAS_TOKEN: '/generate-sastoken',
-    GET_CUSTOMER_IMAGE: '/get-customer-image',
-    SAVE_CUSTOMER_IMAGE: '/save-customer-image',
-};
